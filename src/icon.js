25c50d0725bf01f48fee5a075a072a18c8f0b43f
George Mamadashvili <georgemamadashvili@gmail.com>
2020-07-28 20:30:40 +0400

    Use Twitch icon from Dashicons
    Fixes: #3.

--- src/icon.js
+++ src/icon.js
@@ -6,8 +6,8 @@ import { Path, SVG } from '@wordpress/components';
 export default {
 	foreground: '#9147ff',
 	src: (
-		<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-			<Path d="M391.17,103.47H352.54v109.7h38.63ZM285,103H246.37V212.75H285ZM120.83,0,24.31,91.42V420.58H140.14V512l96.53-91.42h77.25L487.69,256V0ZM449.07,237.75l-77.22,73.12H294.61l-67.6,64v-64H140.14V36.58H449.07Z" />
+		<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+			<Path d="M2.7 2L2 4.6v11.8h3.2V18H7l1.8-1.6h2.9l5.7-5.2V2H2.7zM16 10.5l-2.5 2.3h-4l-2.2 2v-2H4.2V3.3H16v7.2zm-2.5-4.6h-1.4v3.9h1.4V5.9zm-4 0H8.1v3.9h1.4V5.9z" />
 		</SVG>
 	),
 };
